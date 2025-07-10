387360df63c12a6bf1a490e5fef8fa9095534b51
Mark Volkmann <r.mark.volkmann@gmail.com>
2025-07-10 15:49:11 -0500

    progress

--- demo/tests/wrec.spec.ts
+++ demo/tests/wrec.spec.ts
@@ -12,8 +12,31 @@ test("has title", async ({ page }) => {
 test("vanilla counter works", async ({ page }) => {
   await goTo(page);
   const divSelector = "counter-vanilla >>> div";
+  const span = page.locator(divSelector + "> span").first();
+  await expect(span).toHaveText("3");
+
+  const incBtn = page.locator(divSelector + "> #increment-btn");
+  await incBtn.click();
+  await expect(span).toHaveText("4");
+
+  const decBtn = page.locator(divSelector + "> #decrement-btn");
+  for (let i = 1; i <= 4; i++) {
+    await decBtn.click();
+  }
+  await expect(span).toHaveText("0");
+  await expect(decBtn).toBeDisabled();
+
+  await incBtn.click();
+  await expect(decBtn).toBeEnabled();
+});
+
+/*
+test("wrec counter works", async ({ page }) => {
+  await goTo(page);
+  const divSelector = "counter-wrec >>> div";
   const spanSelector = divSelector + "> span";
-  await expect(page.locator(spanSelector)).toHaveText("3");
+  const span = page.locator(spanSelector).first();
+  await expect(span).toHaveText("3");
   await page.click(divSelector + "> #increment-btn");
   await expect(page.locator(spanSelector)).toHaveText("4");
   const decBtn = await page.locator(divSelector + "> #decrement-btn");
@@ -23,3 +46,4 @@ test("vanilla counter works", async ({ page }) => {
   await expect(page.locator(spanSelector)).toHaveText("0");
   await expect(decBtn).toBeDisabled();
 });
+*/
