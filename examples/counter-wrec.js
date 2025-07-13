7b6d9953830bbb44bb252814983042abc103dc7f
Mark Volkmann <r.mark.volkmann@gmail.com>
2025-07-13 09:34:57 -0500

    progress

--- examples/counter-wrec.js
+++ examples/counter-wrec.js
@@ -26,17 +26,13 @@ class CounterWrec extends Wrec {
   `;
 
   static html = html`
-    <button onClick="decrement" type="button" disabled="this.count === 0">
+    <button onClick="this.count--" type="button" disabled="this.count === 0">
       -
     </button>
     <span>this.count</span>
     <button onClick="this.count++" type="button">+</button>
     <span>this.count < 10 ? "single" : "double"</span> digit
   `;
-
-  decrement() {
-    if (this.count > 0) this.count--;
-  }
 }
 
 CounterWrec.register();
