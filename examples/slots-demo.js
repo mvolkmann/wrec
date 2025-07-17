15e1ec861fad9a0f7fecbe558ae0e8ad53a66a2b
Mark Volkmann <r.mark.volkmann@gmail.com>
2025-07-17 11:44:12 -0500

    added slots example

---
+++ examples/slots-demo.js
@@ -0,0 +1,66 @@
+import Wrec, { css, html } from "../wrec.js";
+
+class SlotsDemo extends Wrec {
+  static properties = {
+    color: { type: String },
+    width: { type: String },
+  };
+
+  static css = css`
+    :host {
+      --border-radius: 1rem;
+      --color: cornflowerblue;
+      --width: 20rem;
+      border: 1px solid var(--color);
+      border-radius: var(--border-radius);
+      display: inline-block;
+      font-family: sans-serif;
+      width: var(--width);
+    }
+
+    .content {
+      padding: 1rem;
+    }
+
+    .footer {
+      background-color: var(--color);
+      border-bottom-left-radius: var(--border-radius);
+      border-bottom-right-radius: var(--border-radius);
+      font-size: 0.7rem;
+      padding: 0.25rem;
+      text-align: center;
+    }
+
+    .header {
+      background-color: var(--color);
+      border-top-left-radius: var(--border-radius);
+      border-top-right-radius: var(--border-radius);
+      font-size: 2rem;
+      font-weight: bold;
+      text-align: center;
+    }
+  `;
+
+  static html = html`
+    <div class="card">
+      <div class="header">
+        <slot name="title"></slot>
+      </div>
+      <div class="content">
+        <slot></slot>
+      </div>
+      <div class="footer">
+        <slot name="footer"></slot>
+      </div>
+    </div>
+  `;
+
+  connectedCallback() {
+    super.connectedCallback();
+    const { color, width } = this;
+    if (color) this.style.setProperty("--color", color);
+    if (width) this.style.setProperty("--width", width);
+  }
+}
+
+SlotsDemo.register();
