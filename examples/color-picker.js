edac9c9f302e55ae43e5ce81d583501ac73916d2
Mark Volkmann <r.mark.volkmann@gmail.com>
2025-07-20 14:15:14 -0500

    added color-demo example

---
+++ examples/color-picker.js
@@ -0,0 +1,45 @@
+import Wrec, { css, html } from "../wrec.js";
+
+class ColorPicker extends Wrec {
+  static properties = {
+    red: { type: Number },
+    green: { type: Number },
+    blue: { type: Number },
+    color: { type: String },
+  };
+
+  static css = css`
+    :host {
+      display: flex;
+      gap: 0.5rem;
+    }
+
+    #sliders {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+    }
+
+    #swatch {
+      background-color: var(--color);
+      height: 5rem;
+      width: 5rem;
+    }
+  `;
+
+  static html = html`
+    <div id="swatch"></div>
+    <div id="sliders">
+      <number-slider label="Red" max="255" value="this.red"></number-slider>
+      <number-slider label="Green" max="255" value="this.green"></number-slider>
+      <number-slider label="Blue" max="255" value="this.blue"></number-slider>
+    </div>
+  `;
+
+  attributeChangedCallback(attrName, oldValue, newValue) {
+    super.attributeChangedCallback(attrName, oldValue, newValue);
+    this.color = `rgb(${this.red}, ${this.green}, ${this.blue})`;
+  }
+}
+
+ColorPicker.register();
