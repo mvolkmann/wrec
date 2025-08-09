daa1eb117cea5755475310e8f6a4910d8c0dbb71
Mark Volkmann <r.mark.volkmann@gmail.com>
2025-08-09 07:32:48 -0500

    added state-demo

---
+++ src/examples/state-demo/component-one.js
@@ -0,0 +1,21 @@
+import Wrec, {css, html} from '../../wrec';
+
+class ComponentOne extends Wrec {
+  static properties = {
+    color: {type: String, value: 'black'},
+    name: {type: String}
+  };
+
+  static css = css`
+    p {
+      color: this.color;
+    }
+  `;
+
+  static html = html`
+    <p>Enter your name below.</p>
+    <labeled-input label="Name" value="this.name"></labeled-input>
+  `;
+}
+
+ComponentOne.register();
---
+++ src/examples/state-demo/component-two.js
@@ -0,0 +1,15 @@
+import Wrec, {css, html} from '../../wrec';
+
+class ComponentTwo extends Wrec {
+  static properties = {
+    color: {type: String, value: 'black'},
+    name: {type: String}
+  };
+
+  static html = html`
+    <p>Hello, <span>this.name</span>!</p>
+    <color-picker color="this.color"></color-picker>
+  `;
+}
+
+ComponentTwo.register();
