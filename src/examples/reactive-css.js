ce60d000827520db0ac954664d033a5042064cfe
Mark Volkmann <r.mark.volkmann@gmail.com>
2025-10-07 14:38:01 -0500

    added reactive-css example

---
+++ src/examples/reactive-css.js
@@ -0,0 +1,20 @@
+import Wrec, {css, html} from '../wrec';
+
+class ReactiveCSS extends Wrec {
+  static properties = {
+    size: {type: Number, value: 18}
+  };
+
+  static css = css`
+    p {
+      font-size: this.size + 'px';
+    }
+  `;
+
+  static html = html`
+    <input type="range" value:input="this.size" />
+    <p>My size is reactive!</p>
+  `;
+}
+
+ReactiveCSS.register();
